273bfd52445a86e3b1809d34035f2f898c7617b6
Jade Chen <jade.chen@stonybrook.edu>
2026-03-08 21:07:13 -0400

    basic server setup from spring initializr

---
+++ redistricting-server/src/main/java/cse416/yankees/RedistrictingServerApplication.java
@@ -0,0 +1,13 @@
+package cse416.yankees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedistrictingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedistrictingServerApplication.class, args);
+	}
+
+}
---
+++ redistricting-server/src/test/java/cse416/yankees/RedistrictingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cse416.yankees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedistrictingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
